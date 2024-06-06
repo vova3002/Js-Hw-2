255df1c0f5aa618dfc7a3d9f0eade9f1dcbc5586
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-06-06 17:49:47 +0300

    completed homework2

--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
-import { error, success } from "@pnotify/core";
-import "@pnotify/core/dist/PNotify.css";
-import "@pnotify/core/dist/BrightTheme.css";
+const ctx = document.getElementById('sales-chart');
+
+const chartData = {
+  labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+  datasets: [
+    {
+      label: "Продажі за останній місяць",
+      data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
+      backgroundColor: "#2196f3",
+      borderColor: "#2196f3",
+      borderWidth: 1,
+    },
+  ],
+};
 
 
-const currentSymbol = document.querySelector("#key")
-const currentKey = document.querySelector("#currentSymbol")
-let currentKeyIndex = 0
-const keys = ["a", "z", ";", "b", "w", "p", "m", "v", "u", "x"]
-const newGameButton = document.querySelector("#newGame")
-
-currentKey.textContent = keys[currentKeyIndex]
-
-
-document.addEventListener("keydown", (e) => {
-  // console.log(e.key)
-  currentSymbol.textContent = e.key
-  console.log(keys[currentKeyIndex], e.key)
-  if (keys[currentKeyIndex] === e.key) {
-    currentKeyIndex += 1
-    currentKey.textContent = keys[currentKeyIndex]
-  } else {
-    error({
-      text: 'Incorrect symbol.'
-    });
-  }
-})
-newGameButton.addEventListener("click", (e) => {
-  currentKeyIndex = 0
-  currentKey.textContent = keys[currentKeyIndex]
-  success({
-    text: "New game"
-  })
-})
+const salesChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: chartData.labels,
+    datasets: chartData.datasets
+  },
+});
