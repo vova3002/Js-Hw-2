986acae7bf12ab7c1514ebca063b0e546ee900ff
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-06-13 20:48:57 +0300

    a

---
+++ dist/index.50cb2061.js
@@ -0,0 +1,8 @@
+const form = document.querySelector(".form");
+const names = {
+};
+form.addEventListener("submit", (e)=>{
+    console.log(e.target.elements);
+});
+
+//# sourceMappingURL=index.50cb2061.js.map
