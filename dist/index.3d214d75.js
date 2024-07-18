32d69926124d7995b888eb089328b76eb918b92c
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-07-18 16:57:40 +0300

    work in progress

--- dist/index.3d214d75.js
+++ dist/index.3d214d75.js
@@ -584,32 +584,42 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"bB7Pu":[function(require,module,exports) {
-// import "./js/task1";
-// import "./js/task2";
-// import "./js/task3";
-var _task4 = require("./js/task4");
-
-},{"./js/task4":"gpJMW"}],"gpJMW":[function(require,module,exports) {
-const form = document.querySelector("#userform");
-const name = document.querySelector("#username");
-const lastname = document.querySelector("#userlastname");
-const telephone = document.querySelector("#usertelephone");
-const email = document.querySelector("#useremail");
+// const username1 = document.querySelector("#1")
+// const userage1 = document.querySelector("#2")
+// const usercountry1 = document.querySelector("#3")
+// const username2 = document.querySelector("#4")
+// const userage2 = document.querySelector("#5")
+// const usercountry2 = document.querySelector("#6")
+const inputer = document.querySelector("input");
+const form = document.querySelector("form");
+const searcher = document.querySelector(".searchStudent");
 const button = document.querySelector("button");
 form.addEventListener("submit", (event)=>{
     event.preventDefault("");
-    console.log(event);
-    const saveData = {
-        username: event.target.elements.name.value,
-        userlastname: event.target.elements.lastname.value,
-        usertelephone: event.target.elements.telephone.value,
-        useremail: event.target.elements.email.value
+    const objectUser = {
+        username1: `${event.target.elements.name1.value}`,
+        userage1: `${event.target.elements.age1.value}`,
+        usercountry1: `${event.target.elements.country1.value}`,
+        username2: `${event.target.elements.name2.value}`,
+        userage2: `${event.target.elements.age2.value}`,
+        usercountry2: `${event.target.elements.country2.value}`
     };
-    const stringify = JSON.stringify(saveData);
-    localStorage.setItem("savedData", stringify);
-    console.log(stringify);
-    return stringify;
+    const convertJSON = JSON.stringify(objectUser);
+    localStorage.setItem("studentsData", convertJSON);
+    return objectUser;
 });
+const getLocalData = localStorage.getItem("studentsData");
+const transferParse = JSON.parse(getLocalData);
+console.log(transferParse);
+const students = [
+    {
+        user: `${transferParse}`,
+        age: `${transferParse}`,
+        country: `${transferParse}`
+    }
+];
+console.log(students);
+searcher.addEventListener("", (event)=>{});
 
 },{}]},["km5uZ","bB7Pu"], "bB7Pu", "parcelRequire0d22")
 
