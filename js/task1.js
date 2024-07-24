6b57f448b5c05fe848f1eeb5af05e61ae06f4773
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-07-24 18:05:46 +0300

    update 2

--- js/task1.js
+++ js/task1.js
@@ -1,7 +1,7 @@
-// let messager = 0
-// let interval = setInterval(function(){
-//   console.log(messager += 1)
-//   if(messager === 5){
-//     clearInterval(interval)
-//   }
-// }, 1000)
+let messager = 0
+let interval = setInterval(function(){
+  console.log(messager += 1)
+  if(messager === 5){
+    clearInterval(interval)
+  }
+}, 1000)
