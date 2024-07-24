0d8950cd5c5729df279ed25c65b3db7971126a4c
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-07-24 14:14:00 +0300

    task 1 and 2 done

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-const form = document.querySelector("form")
-const searcher = document.getElementById("search")
-import studentsJson from './js/students.json';
-
-console.log(...studentsJson)
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault("")
-  console.log('submit')
-  const users = {
-    userId: `user${studentsJson.length + 1}`,
-    userName: event.target.elements.name.value,
-    userAge: event.target.elements.age.value,
-    userCountry: event.target.elements.country.value,
-  }
-  
-  const convertJSON = JSON.stringify([...studentsJson, users])
-  localStorage.setItem("studentsData", convertJSON)
-  return users
-})
-
-const getLocalData = localStorage.getItem("studentsData")
-const transferParse = JSON.parse(getLocalData)
-console.log(transferParse)
-
-searcher.addEventListener("input", (event) => {
-  const inputValue = event.target.value
-  const inputFinder = transferParse.find((student) => student.userName === inputValue)
-  console.log(inputFinder)
-})
+import "./js/task1"
+import "./js/task2"
+import "./js/task3"
+import "./js/task4"
---
+++ js/task1.js
@@ -0,0 +1,7 @@
+// let messager = 0
+// let interval = setInterval(function(){
+//   console.log(messager += 1)
+//   if(messager === 5){
+//     clearInterval(interval)
+//   }
+// }, 1000)
