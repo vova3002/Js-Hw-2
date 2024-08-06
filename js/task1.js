48d8375a5603554a75fa47472d89cc0702efcb07
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-08-06 14:57:34 +0300

    lol

--- js/task1.js
+++ js/task1.js
@@ -1,16 +1,12 @@
-// const delay = ms => {
-//   return new Promise(
-//     (resolve) => {
-//       setTimeout(() => {
-//         resolve(`${ms}`)
-//       }, ms)
-//     }
-//   )
-// };
-
-// const logger = time => console.log(`Resolved after ${time}ms`);
-
-// // Виклич функції для перевірки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+// function delayedPromise(text, delay) {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(text), delay);
+//   });
+// }
+// const promise1 = delayedPromise("test", 4000);
+// const promise2 = delayedPromise("tests", 5000);
+// const promise3 = delayedPromise("testing", 6000);
+// const promise4 = delayedPromise("testers", 3000);
+// const promise5 = delayedPromise("tester", 2000);
+// Promise.all([promise1, promise2, promise3, promise4, promise5]).then(value => console.log(value))
+// .catch(error => console.log(error));
