3ed8b95dcd63f2ca7a263cb0e72dba485f2d9c9c
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-08-06 14:59:29 +0300

    oopsie daisy

--- js/task1.js
+++ js/task1.js
@@ -1,12 +1,12 @@
-// function delayedPromise(text, delay) {
-//   return new Promise(resolve => {
-//     setTimeout(() => resolve(text), delay);
-//   });
-// }
-// const promise1 = delayedPromise("test", 4000);
-// const promise2 = delayedPromise("tests", 5000);
-// const promise3 = delayedPromise("testing", 6000);
-// const promise4 = delayedPromise("testers", 3000);
-// const promise5 = delayedPromise("tester", 2000);
-// Promise.all([promise1, promise2, promise3, promise4, promise5]).then(value => console.log(value))
-// .catch(error => console.log(error));
+function delayedPromise(text, delay) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(text), delay);
+  });
+}
+const promise1 = delayedPromise("test", 4000);
+const promise2 = delayedPromise("tests", 5000);
+const promise3 = delayedPromise("testing", 6000);
+const promise4 = delayedPromise("testers", 3000);
+const promise5 = delayedPromise("tester", 2000);
+Promise.all([promise1, promise2, promise3, promise4, promise5]).then(value => console.log(value))
+.catch(error => console.log(error));
