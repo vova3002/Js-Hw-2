43a965fd19ba60610172740b918bd7208e991afc
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-08-27 14:02:28 +0300

    pagination

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const filmEndpoint = "http://localhost:3000/film"
-const movieLIST = document.querySelector(".movie__list")
-
-function getMovie(){
-  const filmData = fetch(`${filmEndpoint}`).then((data) => {
-    console.log(data)
-    return data.json()
-  })
-  return filmData
-}
-
-getMovie().then((movieData) => {
-  const movieHTML = movieData.map((moviesArray) => {
-    console.log(movieData)
-    const moviesSctructure = `<li>
-    <p>${moviesArray.filmName}</p>
-    <img src=${moviesArray.filmImage}>
-    <p>${moviesArray.filmDescription}</p>
+let currentPage = 1
+const button = document.querySelector("button")
+const ulList = document.querySelector(".pixabay__list")
+button.addEventListener("click", () => {
+  currentPage += 1
+  getFetch().then((data) => {
+    const mapArrayHTML = data.hits.map((mapArray) => {
+      console.log(mapArray)
+    let imgHTML = `<li class="pixabay__item">
+    <p class="pixabay__id">Id: ${mapArray.id}</p>
+    <p class="pixabay__likes">Likes: ${mapArray.likes}</p>
+    <img class="pixabay__img" src=${mapArray.userImageURL}>
+    <p class="pixabay__tags">Picture made: ${mapArray.tags}</p>
     </li>`
-    return moviesSctructure
-  })
-  movieLIST.innerHTML = movieHTML
+    return imgHTML
+    })
+      ulList.innerHTML = mapArrayHTML
+    })
 })
+function getFetch() {
+  const fetcher = fetch(`https://pixabay.com/api/?key=43028857-0f1ea2a07ec80ac5b3d9eeb9b&page=${currentPage}&limit=5`).then((data) => data.json())
+  return fetcher
+}
+getFetch().then((data) => {
+  const mapArrayHTML = data.hits.map((mapArray) => {
+    console.log(mapArray)
+  let imgHTML = `<li class="pixabay__item">
+  <p class="pixabay__id">Id: ${mapArray.id}</p>
+  <p class="pixabay__likes">Likes: ${mapArray.likes}</p>
+  <img class="pixabay__img" src=${mapArray.userImageURL}>
+  <p class="pixabay__tags">Picture made: ${mapArray.tags}</p>
+  </li>`
+  return imgHTML
+  })
+    ulList.innerHTML = mapArrayHTML
+  })
